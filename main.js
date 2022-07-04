c9935a382fd5c1a858c34232ed3c6e93eac4dd92
Tornike2002 <butiashvili.t@gtu.ge>
2022-07-04 16:56:38 +0400

    first commit

---
+++ main.js
@@ -0,0 +1,50 @@
+// EASY
+// NUMBER TYPE
+let x = 30;
+console.log(typeof x);
+
+// ARRAY
+let kick = 23;
+let free = 20;
+let man = 11;
+let z = [23, 20, 11];
+console.log(typeof z);
+
+// string 
+
+let stri = 'gamoaqvs stringi'
+console.log(typeof stri);
+
+// boolean
+
+let check = false;
+console.log(typeof check);
+
+// object 
+
+let one = 500;
+let two = 30;
+let three = { one, two };
+console.log(typeof three);
+
+// davaleba 2
+
+let numOne = 520;
+let numTwo = 120;
+let numNum = numOne + numTwo;
+console.log(numNum);
+
+let numNumNum = numOne - numTwo;
+console.log(numNumNum);
+
+let numNumNumNum = numOne / numTwo;
+console.log(numNumNumNum);
+
+let numNumNumNumNum = numOne * numTwo;
+console.log(numNumNumNumNum);
+
+// mesame davaleba
+
+let X = 10;
+let Z = true;
+console.log(X < 12 || X !== 9);
